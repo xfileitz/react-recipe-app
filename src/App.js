1d648ce076c700f0530bf426231a9ace6ed6bf7c
alice <azhang@strongwell.com>
2019-11-06 10:43:41 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 class App extends Component {
   render() {
-    return <div className="App">Hello from our recipe app</div>;
+    return (
+      <Router>
+        <main>
+          {/*navbar here*/}
+          <Navbar />
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" component={SingleRecipe} />
+
+            <Route Component={Default} />
+            {/* <div className="App">
+              <Home />
+              <Recipes />
+
+              <SingleRecipe />
+              <Default />
+            </div> */}
+          </Switch>
+        </main>
+      </Router>
+    );
   }
 }
 
---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SingleRecipe extends Component {
+  render() {
+    return (
+      <div>
+        <h4>hello from single page</h4>
+      </div>
+    );
+  }
+}
