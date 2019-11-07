9ec189bbacb679e90f64f529aeb685694312a268
alice <azhang@strongwell.com>
2019-11-07 08:53:46 -0500

    4nd commit

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,11 +3,22 @@ import Recipe from "./Recipe";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        <h2>hello from recipelist</h2>
-        <Recipe />
-      </div>
+      <>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          <div className="row">
+            {recipes.map(recipe => (
+              <Recipe key={recipe.recipe_id} recipe={recipe} />
+            ))}
+          </div>
+        </div>
+      </>
     );
   }
 }
